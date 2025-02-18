a235efac6fdfadfb4276692cafe62e394acb2e79
Srisai Sudharam <saisudharam1@gmail.com>
2025-02-18 13:01:07 +0530

    Navigate between login and signup pages

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import { Link } from "react-router-dom";
 import LoginForm from "../../components/LoginForm";
 const LoginPage = () => {
   return (
@@ -6,6 +7,11 @@ const LoginPage = () => {
       <div className="login-form ">
         <h1 className="mb-5">Login</h1>
         <LoginForm />
+        <div className="text-end">
+          <Link to="/signup" replace={true}>
+            New User?
+          </Link>
+        </div>
       </div>
     </div>
   );
